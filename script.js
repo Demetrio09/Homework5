1460d708d68820cd0ce60367ebe3ffee145be093
Demetrio09 <demetrioalmeida@gmail.com>
2020-04-21 23:57:46 -0400

    Homework

---
+++ script.js
@@ -0,0 +1,15 @@
+var fullDate = new Date();
+var localHour = fullDate.toLocaleTimeString();
+var monthDayYear = fullDate.toLocaleDateString();
+var day = fullDate.getDay();
+var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayOfWeek = weekDay[day];
+var todayDate = dayOfWeek + ", " + monthDayYear + ", " + localHour;
+
+
+
+
+console.log(localHour);
+console.log(monthDayYear);
+console.log(dayOfWeek);
+console.log(todayDate);
