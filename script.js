8c62021e259d6c0b35339c65b5eb974d868bfb8b
Demetrio <demetrioalmeida@gmail.com>
2020-04-24 01:19:43 -0400

    Homework

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ var localHour = utcDate.toLocaleTimeString();
 var monthDayYear = utcDate.toLocaleDateString();
 var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var dayOfWeek = weekDay[dayIndex];
-var todayDate = dayOfWeek + ", " + monthDayYear + ", " + localHour;
-var inputEl = "";//$(".description");
+var currentlyDate = dayOfWeek + ", " + monthDayYear + ", " + localHour;
+var inputEl = ""//$(".description");
+var inputForm = $(".col-8");
 var dataValue = "";
+var saveButton = $(".saveBtn");
 
 // Show currently date on HTML document.
-$("#currentDay").append(todayDate);
+$("#currentDay").append(currentlyDate);
 
-//get data-value from input tags in order to set attributes.
+// Compare the time you get to the 9AM-5PM
+    // highlight those before the hour green
+    // highlight current hour red
+    // highlight after hour gray
+    // add or remove classes with jQuery
 
 for (i = 0; i <= 8; i++) {
     var inputEl = $(".description" + i);
@@ -28,23 +34,23 @@ for (i = 0; i <= 8; i++) {
     }
 };
 
-console.log(dataValue);
+$("saveButton").on("click", function(event) {
+    event.preventDefault();
 
-// add class to set attributes to input element using if statments
+    var inputElText = inputEl.val().trim();
 
-// function attributesEl() {
-//     if (utcHour > dataValue) {
-//         inputEl.addClass("past");
-//     } else if (utcHour === dataValue) {
-//         inputEl.addClass("present");
-//     } else {
-//         inputEl.addClass("future")
-//     }};
+    localStorage.setItem("input", JSON.stringify(inputElText));
 
-// attributesEl();
+    var lastData = JSON.parse(localStorage.getItem("input"));
+
+    console.log(inputElText);
+    
+    console.log(lastData);
+    
+});
 
 console.log(localHour);
 console.log(monthDayYear);
 console.log(dayOfWeek);
-console.log(todayDate);
+console.log(currentlyDate);
 console.log(utcHour);
