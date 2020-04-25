6cacd000ba300f674d2edbebe859ee9d563d30de
Demetrio <demetrioalmeida@gmail.com>
2020-04-25 14:03:32 -0400

    Homework

--- script.js
+++ script.js
@@ -11,20 +11,29 @@ var inputForm = $(".col-8");
 var dataValue = "";
 var saveButton = $(".saveBtn");
 
+
 // Show currently date on HTML document.
 $("#currentDay").append(currentlyDate);
 
 // Compare the time you get to the 9AM-5PM
-    // highlight those before the hour green
-    // highlight current hour red
-    // highlight after hour gray
-    // add or remove classes with jQuery
+// highlight those before the hour green
+// highlight current hour red
+// highlight after hour gray
+// add or remove classes with jQuery
 
 for (i = 0; i <= 8; i++) {
     var inputEl = $(".description" + i);
     var dataValue = $(".description" + i).attr("data-value");
     console.log(dataValue);
-
+    
+    
+    if (monthDayYear === localStorage.getItem("currentlyDate")) {
+        // Load text for that day
+        $(".description" + i).val(localStorage.getItem(i + 9));
+    } else {
+        localStorage.clear();
+        localStorage.setItem("currentlyDate", monthDayYear);
+    }
     if (utcHour < dataValue) {
         inputEl.addClass("future");
     } else if (utcHour > dataValue) {
@@ -34,20 +43,33 @@ for (i = 0; i <= 8; i++) {
     }
 };
 
-$("saveButton").on("click", function(event) {
+
+$(".saveBtn").on("click", function(event) {
     event.preventDefault();
 
-    var inputElText = inputEl.val().trim();
+    var hour = $(this).siblings(".user-text").attr("data-value");
+    console.log(hour);
 
-    localStorage.setItem("input", JSON.stringify(inputElText));
 
-    var lastData = JSON.parse(localStorage.getItem("input"));
+    var userText = $(this).siblings(".user-text").val();
+    console.log(userText);
 
-    console.log(inputElText);
-    
-    console.log(lastData);
+
+    // console.log($(this).attr("data-text"));
+    // console.log($("." + $(this).attr("data-text")).val());
+
+
+    localStorage.setItem(hour, userText);
+
+    // var inputElText = inputEl.val().trim();
+
+    // localStorage.setItem("input", JSON.stringify(inputElText));
+
+    // var lastData = JSON.parse(localStorage.getItem("input"));
+
     
 });
+//     var inputSave = localStorage.getItem("inputSave");
 
 console.log(localHour);
 console.log(monthDayYear);
